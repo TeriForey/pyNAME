3e23af3bb64417a645fe9332ec8790005a74787d
Teri Forey <trf5@le.ac.uk>
2018-09-18 15:42:14 +0100

    Removed groups from plotting cycle

--- src/pyname/create_plots.py
+++ src/pyname/create_plots.py
@@ -12,12 +12,14 @@ def plot_all(inputs, runid):
     :return:
     """
 
-    rundir = os.path.join(inputs['outputdir'], runid)
+    rundir = inputs['outputdir']
 
     # Parse input params into plot options
     plotoptions = {'station': (inputs['longitude'], inputs['latitude'])}
 
     plotoptions['outdir'] = os.path.join(rundir, "plots")
+    print plotoptions
+    print rundir
 
     files = glob.glob(os.path.join(rundir, 'outputs', '*_group*.txt'))
     if len(files) == 0:
@@ -40,22 +42,19 @@ def plot_all(inputs, runid):
             groups[groupnum] = tempfile.mkdtemp()
             shutil.copy(os.path.join(rundir, 'outputs', filename), groups[groupnum])
 
-    for groupnum, tmpdir in sorted(groups.items()):
-        for filename in os.listdir(tmpdir):
-            if '_group' in filename and filename.endswith('.txt'):
-                n = Name(os.path.join(tmpdir, filename))
-                for column in n.timestamps:
-                    plotoptions['outfile'] = "name_%s_%s_%s_%sdayback_%sm.png" % (inputs['title'],
+    #for groupnum, tmpdir in sorted(groups.items()):
+    for filename in os.listdir(os.path.join(rundir, 'outputs')):
+        if '_group' in filename and filename.endswith('.txt'):
+            n = Name(os.path.join(rundir, 'outputs', filename))
+            for column in n.timestamps:
+                plotoptions['outfile'] = "name_%s_%s_%s_%sdayback_%sm.png" % (inputs['title'],
                                                                                   filename.split('_')[0],
                                                                                   column.split(' ')[1].replace(':', ''),
                                                                                   inputs['time'],
                                                                                   inputs['elevation_out'])
-                    try:
-                        drawMap(n, column, **plotoptions)
-                    except:
-                        print("Failed to plot %s" % column)
-
-        # Finished plotting so will now delete temp directory
-        shutil.rmtree(tmpdir)
+                try:
+                    drawMap(n, column, **plotoptions)
+                except:
+                    print("Failed to plot %s" % column)
 
     return
